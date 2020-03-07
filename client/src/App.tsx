f0f80036f5e2ac04c79327a1e9d528de6f232825
Maxime Antoine <maximeantoine@MacBook-Pro-de-Maxime.local>
2020-03-07 08:23:48 +0100

    Add basic React Router to App page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Reservation_Form from "./components/reservation_form";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    links: {
+      flexGrow: 1,
+      flexDirection: "row",
+      alignContent: "end"
+    },
+    link: {
+      color: "black",
+      padding: "25px",
+      font: "Roboto",
+      fontSize: "20px",
+      textDecoration: "none",
+      "&:hover": {
+        color: "grey",
+        transition: "0.2s"
+      }
+    },
+    logo: {
+      color: "black",
+      fontWeight: "bold",
+      padding: "25px",
+      font: "Roboto",
+      fontSize: "20px",
+      textDecoration: "none",
+      "&:hover": {
+        color: "grey",
+        transition: "0.2s"
+      }
+    }
+  })
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static" color="inherit">
+        <Toolbar variant="regular">
+          <Link to="/" className={classes.logo}>
+            Antoine SPRL
+          </Link>
+          <Link to="/calendrier" className={classes.link}>
+            Calendrier
+          </Link>
+          <Link to="/reservation" className={classes.link}>
+            Reservation
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/reservation">
+          <Reservation_Form />
+        </Route>
+        <Route path="/calendrier">
+          <>CALENDRIER</>
+        </Route>
+        <Route path="/">
+          <>HOME</>
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
