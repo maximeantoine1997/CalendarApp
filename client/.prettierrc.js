9b1e53c7206078a1d7a6b4a0740f766ae3e0c92f
Maxime Antoine <maximeantoine@MacBook-Pro-de-Maxime.local>
2020-03-07 08:22:59 +0100

    Add prettier file to project

---
+++ client/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    printWidth: 120,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "consistent",
+    bracketSpacing: true,
+    arrowParens: "avoid",
+    endOfLine: "lf",
+};
