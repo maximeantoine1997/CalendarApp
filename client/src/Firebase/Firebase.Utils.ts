2bb21a849e46da22263716628f56b3e5dec9d507
Maxime Antoine <maximeantoine1997@gmail.com>
2020-03-18 18:10:55 +0100

    Add firebase utils functions

---
+++ client/src/Firebase/Firebase.Utils.ts
@@ -0,0 +1,58 @@
+import * as firebase from "firebase/app";
+import "firebase/database";
+import { Nullable } from "Interfaces/Common";
+import { isArray } from "util";
+
+export const getFirebaseElementAsync = async (url: string): Promise<string> => {
+   let res: string = "";
+   await firebase
+      .database()
+      .ref(url)
+      .once("value")
+      .then(snapshot => {
+         res = snapshot.val();
+      });
+
+   return res;
+};
+
+export const getFirebaseElementsAsync = async (url: string): Promise<Array<string>> => {
+   const res: Array<string> = [];
+   await firebase
+      .database()
+      .ref(url)
+      .once("value")
+      .then(snapshot => {
+         const values: Array<string> = snapshot.val();
+         values.forEach(value => {
+            if (value) res.push(value);
+         });
+      });
+
+   return res;
+};
+
+export const updateFirebaseElementAsync = async (url: string): Promise<void> => {};
+
+export const deleteFirebaseElementAsync = async (url: string): Promise<void> => {};
+
+export const findFirebaseElementAsync = async <T>(url: string, key: string, value: string): Promise<Nullable<T>> => {
+   let res: Nullable<T> = null;
+   await firebase
+      .database()
+      .ref(url)
+      .orderByChild(key)
+      .equalTo(value)
+      .once("value")
+      .then(snapshot => {
+         const result = snapshot.val();
+         res = result;
+      });
+   return res;
+};
+
+export const getFirebaseKey = (value: any): Nullable<string> => {
+   const keys = Object.keys(value);
+   if (keys.length === 1) return keys[0];
+   return null;
+};
