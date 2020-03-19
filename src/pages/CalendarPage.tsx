030e404fd5a4687037df379485efdb154c6dd058
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-19 06:55:50 +0100

    Add basic layout for page components

---
+++ src/pages/CalendarPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CalendarPage = () => {
+   return (
+      <>
+         <h1> Calendar Page </h1>
+      </>
+   );
+};
+
+export default CalendarPage;
---
+++ src/pages/ReservationPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReservationForm from "components/reservation_form";
+
+const ReservationPage = () => {
+   return (
+      <>
+         <ReservationForm />
+      </>
+   );
+};
+
+export default ReservationPage;
