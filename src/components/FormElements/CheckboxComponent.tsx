504103969d2daac565e8a0f91d67d962205f361f
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-20 07:46:29 +0100

    Add checkbox component

---
+++ src/components/FormElements/CheckboxComponent.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent, ReactElement, useState } from "react";
+import { FormControlLabel, Typography, Switch, Box } from "@material-ui/core";
+
+interface EuroProps {
+   placeholder: string;
+   onChange: (e: boolean) => void;
+}
+
+const CheckBoxComponent: FunctionComponent<EuroProps> = ({ placeholder, onChange }): ReactElement => {
+   const [checked, setChecked] = useState<boolean>(false);
+
+   const onCheck = (event: any, newCheck: boolean) => {
+      onChange(newCheck);
+      console.log(newCheck);
+      setChecked(newCheck);
+   };
+
+   return (
+      <Box width="100%">
+         <FormControlLabel
+            control={<Switch checked={checked} onChange={onCheck} name="Bancontact" color="secondary" />}
+            label={<Typography variant="h6">{placeholder}</Typography>}
+            labelPlacement="start"
+         />
+      </Box>
+   );
+};
+
+export default CheckBoxComponent;
