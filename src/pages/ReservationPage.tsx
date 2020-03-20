c9df877aa88d3ada853d3cc40c1bf7be469de641
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-20 07:47:07 +0100

    Add Box

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReservationForm from "components/reservation_form";
+import { Box } from "@material-ui/core";
 
 const ReservationPage = () => {
    return (
-      <>
+      <Box>
          <ReservationForm />
-      </>
+      </Box>
    );
 };
 
