3fc4ca8659cc6083a4f9af1989312b614ad793fc
Maxime Antoine <maximeantoine1997@gmail.com>
2020-03-22 17:52:06 +0100

    Add styling

--- src/components/FormElements/TextComponent.tsx
+++ src/components/FormElements/TextComponent.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, FunctionComponent, ReactElement, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import { Box, makeStyles, Theme, createStyles, fade } from "@material-ui/core";
+import { makeStyles, createStyles, Grid } from "@material-ui/core";
 import { getFirebaseElementsAsync } from "Firebase/Firebase.Utils";
 
 const useStyles = makeStyles(() =>
    createStyles({
-      color: {
-         backgroundColor: "white",
+      textbox: {
+         backgroundColor: "#E7EAED",
       },
+      autocomplete: {
+         width: "90%",
+      },
+      grid: {},
    })
 );
 
@@ -59,27 +63,26 @@ const TextComponent: FunctionComponent<TextProps> = ({
    }, [options.length, options_, sortBy, url]);
 
    return (
-      <Box width="75%">
-         <Autocomplete
-            multiple={multiple}
-            freeSolo
-            disableClearable
-            options={options.sort((a, b) => -b.charAt(0).localeCompare(a.charAt(0)))}
-            groupBy={option => option.charAt(0).toUpperCase()}
-            renderInput={params => (
-               <TextField
-                  {...params}
-                  label={placeholder}
-                  margin="normal"
-                  variant="outlined"
-                  InputProps={{ ...params.InputProps, type: "search" }}
-                  fullWidth
-                  className={classes.color}
-               />
-            )}
-            onBlur={event => onBlur(event as any)}
-         />
-      </Box>
+      <Autocomplete
+         className={classes.autocomplete}
+         multiple={multiple}
+         freeSolo
+         disableClearable
+         options={options.sort((a, b) => -b.charAt(0).localeCompare(a.charAt(0)))}
+         groupBy={option => option.charAt(0).toUpperCase()}
+         renderInput={params => (
+            <TextField
+               {...params}
+               label={placeholder}
+               fullWidth
+               margin="normal"
+               variant="outlined"
+               InputProps={{ ...params.InputProps, type: "search" }}
+               className={classes.textbox}
+            />
+         )}
+         onBlur={event => onBlur(event as any)}
+      />
    );
 };
 
