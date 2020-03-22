9470ecae26057cbc7876d1b110ae3b935e644430
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-22 17:52:21 +0100

    Add styling to CalendarPage

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { makeStyles, createStyles, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles(() =>
+   createStyles({
+      grid: {
+         height: "95vh",
+         paddingTop: "3vh",
+      },
+   })
+);
 
 const CalendarPage = () => {
+   const classes = useStyles();
    return (
-      <>
+      <Grid container xs={12} justify="center" alignContent="center" alignItems="center" className={classes.grid}>
          <h1> Calendar Page </h1>
-      </>
+      </Grid>
    );
 };
 
