611aa7e51c2322c398a0d8b515fef357e344b708
Maxime Antoine <maximeantoine1997@gmail.com>
2020-03-22 17:52:30 +0100

    Add basic styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { makeStyles, createStyles, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles(() =>
+   createStyles({
+      grid: {
+         height: "95vh",
+         paddingTop: "3vh",
+      },
+   })
+);
 
 const HomePage = () => {
+   const classes = useStyles();
    return (
-      <>
+      <Grid container xs={12} justify="center" alignContent="center" alignItems="center" className={classes.grid}>
          <h1> Home Page </h1>
-      </>
+      </Grid>
    );
 };
 
