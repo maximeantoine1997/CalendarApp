8e6b7f88d286e137aa91b40d667f2b4c3b614ff5
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-22 17:52:37 +0100

    Add styling

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReservationForm from "components/reservation_form";
-import { Box } from "@material-ui/core";
+import { makeStyles, Theme, createStyles, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+   createStyles({
+      grid: {
+         height: "100%",
+         paddingTop: "3vh",
+      },
+   })
+);
 
 const ReservationPage = () => {
+   const classes = useStyles();
    return (
-      <Box>
+      <Grid container xs={12} justify="center" alignContent="center" alignItems="center" className={classes.grid}>
          <ReservationForm />
-      </Box>
+      </Grid>
    );
 };
 
