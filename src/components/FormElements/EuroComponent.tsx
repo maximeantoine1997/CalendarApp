76aef83e50cb888fa82330a19ed72ab64ac6a55b
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-22 17:51:57 +0100

    Add styling

--- src/components/FormElements/EuroComponent.tsx
+++ src/components/FormElements/EuroComponent.tsx
@@ -1,7 +1,15 @@
 import React, { FunctionComponent, ReactElement } from "react";
 import TextField from "@material-ui/core/TextField";
-import { Box, Grid } from "@material-ui/core";
+import { Box, Grid, makeStyles, createStyles } from "@material-ui/core";
 import EuroIcon from "@material-ui/icons/Euro";
+
+const useStyles = makeStyles(() =>
+   createStyles({
+      color: {
+         backgroundColor: "#E7EAED",
+      },
+   })
+);
 
 interface EuroProps {
    placeholder: string;
@@ -9,6 +17,7 @@ interface EuroProps {
 }
 
 const EuroComponent: FunctionComponent<EuroProps> = ({ placeholder, onChange }): ReactElement => {
+   const classes = useStyles();
    const onBlur = (event: any) => {
       if (event) {
          const newValue = event.target.value;
@@ -24,7 +33,14 @@ const EuroComponent: FunctionComponent<EuroProps> = ({ placeholder, onChange }):
                <EuroIcon fontSize="large" />
             </Grid>
             <Grid item>
-               <TextField label={placeholder} onBlur={onBlur} variant="outlined" fullWidth margin="normal" />
+               <TextField
+                  label={placeholder}
+                  onBlur={onBlur}
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  className={classes.color}
+               />
             </Grid>
          </Grid>
       </Box>
