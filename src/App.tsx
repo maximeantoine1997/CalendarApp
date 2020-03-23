c730c32c1aec3c3cb9a6d3133ef8bf191abd11da
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-23 08:31:34 +0100

    Add user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { makeStyles, Theme, createStyles, Box, Grid } from "@material-ui/core";
+import { makeStyles, Theme, createStyles, Grid } from "@material-ui/core";
 import * as firebase from "firebase/app";
 import "firebase/database";
 import ReservationPage from "pages/ReservationPage";
 import CalendarPage from "pages/CalendarPage";
 import HomePage from "pages/HomePage";
-import SideBar from "components/Sidebar";
+import SideBar from "components/Navigation/Sidebar";
 import AddButton from "components/AddButton";
+import { UserContextProvider } from "Contexts/UserContext";
 
 const useStyles = makeStyles((theme: Theme) =>
    createStyles({
@@ -39,28 +40,30 @@ const App: React.FC = () => {
    const classes = useStyles();
 
    return (
-      <Router>
-         <Grid container xs={12}>
-            <Grid item xs={12} className={classes.menu}>
-               <SideBar />
+      <UserContextProvider>
+         <Router>
+            <Grid container xs={12}>
+               <Grid item xs={12} className={classes.menu}>
+                  <SideBar />
+               </Grid>
+               <Grid item xs={12} className={classes.content}>
+                  <AddButton>
+                     <Switch>
+                        <Route path="/reservation">
+                           <ReservationPage />
+                        </Route>
+                        <Route path="/calendrier">
+                           <CalendarPage />
+                        </Route>
+                        <Route path="/">
+                           <HomePage />
+                        </Route>
+                     </Switch>
+                  </AddButton>
+               </Grid>
             </Grid>
-            <Grid item xs={12} className={classes.content}>
-               <AddButton>
-                  <Switch>
-                     <Route path="/reservation">
-                        <ReservationPage />
-                     </Route>
-                     <Route path="/calendrier">
-                        <CalendarPage />
-                     </Route>
-                     <Route path="/">
-                        <HomePage />
-                     </Route>
-                  </Switch>
-               </AddButton>
-            </Grid>
-         </Grid>
-      </Router>
+         </Router>
+      </UserContextProvider>
    );
 };
 
