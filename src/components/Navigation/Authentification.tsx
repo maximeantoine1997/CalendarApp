74e1d42e5f94cb65705d270a15e7c77b1ca3595b
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-25 05:52:59 +0100

    Add authentification with firebase

--- src/components/Navigation/Authentification.tsx
+++ src/components/Navigation/Authentification.tsx
@@ -8,10 +8,16 @@ import {
    Typography,
    TextField,
    Button,
+   Avatar,
+   Menu,
+   MenuItem,
+   Fade,
 } from "@material-ui/core";
-import React, { useRef } from "react";
-import firebase from "firebase";
-import { signInFirebase } from "Firebase/Firebase.Utils";
+import React, { useRef, useState, useEffect } from "react";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import useUserContext from "Contexts/UserContext";
+import Transition from "components/Transitions/Transition";
 
 const useStyles = makeStyles((theme: Theme) =>
    createStyles({
@@ -43,61 +49,138 @@ const useStyles = makeStyles((theme: Theme) =>
          background: "linear-gradient(to right, #606c88, #3f4c6b)",
          color: "white",
       },
+      profileMenu: {
+         marginTop: "50px",
+      },
    })
 );
 
-interface AuthProps {
-   onToggle: (value: boolean) => void;
-   open: boolean;
-}
+interface AuthProps {}
 
-const Authentification: React.FC<AuthProps> = ({ onToggle, open }) => {
+const Authentification: React.FC<AuthProps> = () => {
+   console.log("Auth component rendering");
+
    const classes = useStyles();
 
-   const email = useRef("");
-   const password = useRef("");
+   const email = useRef("florian@antoinesprl.be");
+   const password = useRef("123456");
+   const userName = useRef("");
 
-   const onBlur = (event: any, ref: React.MutableRefObject<string>): void => {
-      ref.current = event.target.value;
-   };
+   const [isOpenAuth, setIsOpenAuth] = useState<boolean>(false);
+   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
    const onSignUp = (): void => {
       console.log(`Sign Up => email is: ${email.current} & password is: ${password.current}`);
       firebase
          .auth()
          .createUserWithEmailAndPassword(email.current, password.current)
+         .then(value => {
+            value.user
+               ?.updateProfile({
+                  displayName: userName.current,
+               })
+               .then(() => console.log("it worked"))
+               .catch(error => console.error(error));
+         })
          .catch(error => {
-            // Handle Errors here.
-            // const errorCode = error.code;
             const errorMessage = error.message;
             console.error(errorMessage);
-            // ...
          });
-      onToggle(false);
+      setIsOpenAuth(false);
    };
 
    const onSignIn = (): void => {
       console.log(`Sign In => email is: ${email.current} & password is: ${password.current}`);
-      signInFirebase(email, password);
-      onToggle(false);
+      firebase
+         .auth()
+         .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+         .then(async () => {
+            await firebase.auth().signInWithEmailAndPassword(email.current, password.current);
+            console.log("signed in");
+            setIsOpenAuth(false);
+         })
+         .catch(error => {
+            var errorMessage = error.message;
+            console.error(errorMessage);
+         });
    };
 
-   return (
-      <Dialog open={open} fullWidth onClose={() => onToggle(false)}>
-         <DialogTitle id="simple-dialog-title">Se Connecter</DialogTitle>
-         <DialogContent>
-            <Typography variant="h6">Sign In</Typography>
-            <TextField variant="outlined" label="email" onBlur={e => onBlur(e, email)} />
-            <TextField variant="outlined" label="password" onBlur={e => onBlur(e, password)} />
-            <Button variant="contained" onClick={() => onSignUp()}>
-               Sign up
+   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+   };
+
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
+
+   const toggleAuth = (value: boolean) => {
+      setIsOpenAuth(value);
+   };
+
+   const onBlur = (event: any, ref: React.MutableRefObject<string>): void => {
+      ref.current = event.target.value;
+   };
+
+   useEffect(() => {
+      console.log("render");
+   }, []);
+
+   const { user } = useUserContext();
+   const initial = user?.displayName?.charAt(0).toUpperCase();
+   if (user && initial) {
+      return (
+         <>
+            <Button variant="text" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+               <Avatar alt="User Avatar">{initial}</Avatar>
             </Button>
-            <Button variant="contained" onClick={() => onSignIn()}>
-               Sign In
-            </Button>
-         </DialogContent>
-      </Dialog>
-   );
+            <Menu
+               id="simple-menu"
+               anchorEl={anchorEl}
+               keepMounted
+               open={Boolean(anchorEl)}
+               onClose={handleClose}
+               className={classes.profileMenu}
+            >
+               <MenuItem onClick={handleClose}>Profile</MenuItem>
+               <MenuItem onClick={handleClose}>Se Déconnecter</MenuItem>
+            </Menu>
+         </>
+      );
+   }
+   if (isOpenAuth) {
+      return (
+         <Dialog open={isOpenAuth} fullWidth onClose={() => setIsOpenAuth(false)}>
+            <DialogTitle id="simple-dialog-title">Se Connecter</DialogTitle>
+            <DialogContent>
+               <Typography variant="h6">Sign In</Typography>
+               <TextField variant="outlined" label="email" onBlur={e => onBlur(e, email)} />
+               <TextField variant="outlined" label="password" onBlur={e => onBlur(e, password)} />
+               <TextField variant="outlined" label="username" onBlur={e => onBlur(e, userName)} />
+               <Button variant="contained" onClick={() => onSignUp()}>
+                  Sign up
+               </Button>
+               <Button variant="contained" onClick={() => onSignIn()}>
+                  Sign In
+               </Button>
+            </DialogContent>
+         </Dialog>
+      );
+   }
+
+   if (user === null) {
+      return (
+         <Button
+            color="inherit"
+            onClick={() => {
+               toggleAuth(true);
+            }}
+         >
+            Se Connecter
+         </Button>
+      );
+   }
+
+   return <></>;
 };
 
 export default Authentification;
