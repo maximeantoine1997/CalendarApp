5f445a428428476e40fd87963340e5dbb908ac31
Maxime Antoine <maximeantoine1997@gmail.com>
2020-03-27 07:58:33 +0100

    Add redirection if no signed user

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReservationForm from "components/reservation_form";
 import { makeStyles, Theme, createStyles, Grid } from "@material-ui/core";
+import useUserContext from "Contexts/UserContext";
+import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
    createStyles({
       grid: {
          height: "100%",
+         // This makes sure that the content doesn't go under the navbar
          paddingTop: "3vh",
       },
    })
@@ -13,11 +16,18 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const ReservationPage = () => {
    const classes = useStyles();
-   return (
-      <Grid container justify="center" alignContent="center" alignItems="center" className={classes.grid}>
-         <ReservationForm />
-      </Grid>
-   );
+
+   const { user } = useUserContext();
+
+   if (user) {
+      return (
+         <Grid container justify="center" alignContent="center" alignItems="center" className={classes.grid}>
+            <ReservationForm />
+         </Grid>
+      );
+   }
+
+   return <Redirect to="/" />;
 };
 
 export default ReservationPage;
