c599d972cecec42383a1f659a43ea867ea32afeb
Maxime Antoine <maximeantoine@MBP-de-Maxime.lan>
2020-03-27 07:58:54 +0100

    Add checkIfConnected

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,11 @@
+import { Optional, Nullable } from "Interfaces/Common";
+
 export const isFunction = (f: any): f is Function => {
    return typeof f === "function";
+};
+
+export const checkIfConnected = (user: Optional<Nullable<firebase.User>>, path: string): string => {
+   if (!user) return "/";
+
+   return path;
 };
