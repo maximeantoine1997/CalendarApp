c3e9225b37efd1caa006c3def6a11514e34c6c3d
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-05 14:41:57 +0200

    Update styling

--- src/components/FormElements/TextComponent.tsx
+++ src/components/FormElements/TextComponent.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, FunctionComponent, ReactElement, useState } from "react";
 import TextField from "@material-ui/core/TextField";
+import { makeStyles, createStyles, Typography } from "@material-ui/core";
+
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import { makeStyles, createStyles } from "@material-ui/core";
-import { getFirebaseElementsAsync } from "Firebase/Firebase.Utils";
+import { getFirebaseElementsAsync } from "../../Firebase/Firebase.Utils";
 
 const useStyles = makeStyles(() =>
    createStyles({
@@ -14,17 +15,24 @@ const useStyles = makeStyles(() =>
          paddingRight: "10%",
          width: "80%",
       },
-      grid: {},
+      errorText: {
+         paddingLeft: "10%",
+         paddingRight: "10%",
+         width: "80%",
+         color: "red",
+      },
    })
 );
 
 interface TextProps {
    placeholder: string;
-   onChange: (e: string) => void;
+   onChange: (e: string | Array<string>) => void;
    options?: Array<string>;
    url?: string;
    sortBy?: string;
    multiple?: boolean;
+   hasError?: boolean;
+   errorText?: string;
 }
 
 const TextComponent: FunctionComponent<TextProps> = ({
@@ -34,6 +42,8 @@ const TextComponent: FunctionComponent<TextProps> = ({
    onChange,
    sortBy,
    multiple = false,
+   hasError,
+   errorText,
 }): ReactElement => {
    const classes = useStyles();
    const [options, setOptions] = useState<Array<string>>(options_);
@@ -42,7 +52,13 @@ const TextComponent: FunctionComponent<TextProps> = ({
       if (event) {
          const newValue = event.target.value;
          onChange(newValue);
-         console.log(newValue);
+      }
+   };
+
+   const onChangeMultiple = (value: any) => {
+      if (multiple) {
+         onChange(value);
+         console.log(value);
       }
    };
 
@@ -65,26 +81,31 @@ const TextComponent: FunctionComponent<TextProps> = ({
    }, [options.length, options_, sortBy, url]);
 
    return (
-      <Autocomplete
-         className={classes.autocomplete}
-         multiple={multiple}
-         freeSolo
-         disableClearable
-         options={options.sort((a, b) => -b.charAt(0).localeCompare(a.charAt(0)))}
-         groupBy={option => option.charAt(0).toUpperCase()}
-         renderInput={params => (
-            <TextField
-               {...params}
-               label={placeholder}
-               fullWidth
-               margin="normal"
-               variant="outlined"
-               InputProps={{ ...params.InputProps, type: "search" }}
-               className={classes.textbox}
-            />
-         )}
-         onBlur={event => onBlur(event as any)}
-      />
+      <>
+         <Autocomplete
+            className={classes.autocomplete}
+            multiple={multiple}
+            freeSolo
+            disableClearable
+            options={options.sort((a, b) => -b.charAt(0).localeCompare(a.charAt(0)))}
+            groupBy={option => option.charAt(0).toUpperCase()}
+            renderInput={params => (
+               <TextField
+                  error={hasError}
+                  {...params}
+                  label={placeholder}
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  InputProps={{ ...params.InputProps, type: "search" }}
+                  className={classes.textbox}
+               />
+            )}
+            onBlur={event => onBlur(event as any)}
+            onChange={(event: object, value: any) => onChangeMultiple(value)}
+         />
+         {hasError && <Typography className={classes.errorText}>{errorText}</Typography>}
+      </>
    );
 };
 
