b4fc33bba8e56a1a9af0c8f42eb32477ca230512
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-05 14:42:33 +0200

    Add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+// #region Firebase
+
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// #endregion
 
 ReactDOM.render(<App />, document.getElementById("react"));
