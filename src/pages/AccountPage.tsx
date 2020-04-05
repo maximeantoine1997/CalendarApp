5deb860672cc2a75e761eba5cd9987971bee6fe5
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-05 14:44:43 +0200

    Add user context

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles, createStyles, Grid, Typography } from "@material-ui/core";
-import useUserContext from "Contexts/UserContext";
 import { Redirect } from "react-router-dom";
+import useUserContext from "../Contexts/UserContext";
 
 const useStyles = makeStyles(() =>
    createStyles({
