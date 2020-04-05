2e8b5d2a7a1be93c61cb4640cb4132b28db624ed
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-05 14:42:50 +0200

    Remove absolute paths

--- src/Firebase/Firebase.Utils.ts
+++ src/Firebase/Firebase.Utils.ts
@@ -1,7 +1,8 @@
 import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
-import { Nullable } from "Interfaces/Common";
+import { Nullable } from "../Interfaces/Common";
+import { Reservation } from "../components/reservation_form";
 
 // #region Realtime Database
 export const getFirebaseElementAsync = async (url: string): Promise<string> => {
@@ -34,6 +35,10 @@ export const getFirebaseElementsAsync = async (url: string): Promise<Array<any>>
    return res;
 };
 
+export const addFirebaseElementAsync = async (url: string, data: unknown): Promise<void> => {
+   await firebase.database().ref(url).push();
+};
+
 export const updateFirebaseElementAsync = async (url: string): Promise<void> => {};
 
 export const deleteFirebaseElementAsync = async (url: string): Promise<void> => {};
@@ -57,6 +62,60 @@ export const getFirebaseKey = (value: any): Nullable<string> => {
    const keys = Object.keys(value);
    if (keys.length === 1) return keys[0];
    return null;
+};
+
+export const dateExistAsync = async (date: string): Promise<boolean> => {
+   let exist = false;
+   await firebase
+      .database()
+      .ref("Calendar")
+      .orderByKey()
+      .equalTo(date)
+      .once("value")
+      .then(snapshot => {
+         if (snapshot.val()) {
+            exist = true;
+         }
+      })
+      .catch(error => console.error(error));
+
+   return exist;
+};
+
+export const addReservationAsync = async (reservation: Reservation): Promise<void> => {
+   const dateKey = reservation.startDate.format("YYYY-MM-DD");
+   // const dateExist = await dateExistAsync(dateKey);
+
+   const reservationId = firebase.database().ref(`Calendar/${dateKey}`).push().key;
+   if (reservationId) {
+      reservation.id = reservationId;
+
+      const updates: Record<string, string> = {};
+      updates[`Calendar/${dateKey}/${reservationId}`] = JSON.stringify(reservation);
+
+      firebase.database().ref().update(updates);
+   }
+};
+
+export const getReservationsAsync = async (date: string): Promise<Array<Reservation>> => {
+   let reservations: Array<Reservation> = [];
+
+   await firebase
+      .database()
+      .ref(`Calendar/${date}`)
+      .once("value")
+      .then(snapshot => {
+         if (snapshot) {
+            const values = snapshot.val();
+            if (values) {
+               const raw = Object.values(values) as Array<string>;
+               console.log(raw);
+               reservations = raw.map(res => JSON.parse(res));
+            }
+         }
+      });
+
+   return reservations;
 };
 
 // #endregion
