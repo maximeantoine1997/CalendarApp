f40fde1ff7a1b00c0543f5ac289dee84c936363c
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-05 14:42:24 +0200

    Remove absolute paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { makeStyles, Theme, createStyles, Grid } from "@material-ui/core";
 import * as firebase from "firebase/app";
-import "firebase/database";
 import "firebase/auth";
-import ReservationPage from "pages/ReservationPage";
-import CalendarPage from "pages/CalendarPage";
-import HomePage from "pages/HomePage";
-import SideBar from "components/Navigation/Sidebar";
-import AddButton from "components/AddButton";
-import useUserContext, { UserContextProvider } from "Contexts/UserContext";
-import { isFunction } from "Utils";
-import AccountPage from "pages/AccountPage";
+import useUserContext, { UserContextProvider } from "./Contexts/UserContext";
+import { isFunction } from "./Utils";
+import SideBar from "./components/Navigation/Sidebar";
+import AddButton from "./components/AddButton";
+import ReservationPage from "./pages/ReservationPage";
+import CalendarPage from "./pages/CalendarPage";
+import AccountPage from "./pages/AccountPage";
+import HomePage from "./pages/HomePage";
 
 const useStyles = makeStyles((theme: Theme) =>
    createStyles({
@@ -24,22 +23,6 @@ const useStyles = makeStyles((theme: Theme) =>
    })
 );
 
-// #region Firebase
-
-const firebaseConfig = {
-   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// #endregion
-
 // #region react-redux-firebase
 
 const App: React.FC = () => {
@@ -47,12 +30,12 @@ const App: React.FC = () => {
    const { setUser } = useUserContext();
    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-         console.log("user is signed in");
          if (isFunction(setUser)) setUser(user);
       } else {
-         console.log("No User is signed in");
       }
    });
+
+   console.log("App Rendering");
 
    return (
       <UserContextProvider>
