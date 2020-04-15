426616af086f08b0797afad8c58f2a56fbde4041
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-15 10:54:43 +0200

    Update Calendar Modal with read and edit mode

--- src/components/Calendar/CalendarModal.tsx
+++ src/components/Calendar/CalendarModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useRef } from "react";
 import {
    Grid,
    Typography,
@@ -8,8 +8,13 @@ import {
    DialogContent,
    DialogTitle,
    Chip,
+   Button,
+   DialogActions,
+   Divider,
 } from "@material-ui/core";
 import { Reservation } from "../reservation_form";
+import TextComponent from "../FormElements/TextComponent";
+import { updateFirebaseElementAsync, updateReservationAsync } from "../../Firebase/Firebase.Utils";
 
 const useStyles = makeStyles(() =>
    createStyles({
@@ -19,7 +24,7 @@ const useStyles = makeStyles(() =>
       title: {
          paddingTop: "20px",
          fontFamily: "Arial",
-         fontSize: "1.5em",
+         fontSize: "1.3em",
       },
    })
 );
@@ -29,41 +34,184 @@ interface CalendarModalProps {
    onClose: () => void;
 }
 
-const CalendarModal: React.FC<CalendarModalProps> = ({ reservation, open, onClose: onClose_ }) => {
+const CalendarModal: React.FC<CalendarModalProps> = ({ reservation: reservation_, open, onClose: onClose_ }) => {
    const classes = useStyles();
 
-   const renderAccessoires: Array<ReactElement> = reservation.accessoires
-      ? reservation.accessoires.map((accessoire, index) => {
+   const [editMode, setEditMode] = useState<boolean>(false);
+
+   const reservation = useRef<Reservation>(reservation_);
+
+   const editReservation = useRef<Reservation>(reservation_);
+
+   const updateReservation = (type: string, newValue: any) => {
+      const newReservation = { ...editReservation.current };
+
+      //! Add start date and end date changes
+      switch (type) {
+         case "accessoires":
+            newReservation.accessoires = newValue;
+            break;
+         case "address":
+            newReservation.address = newValue;
+            break;
+         case "email":
+            newReservation.email = newValue;
+            break;
+         case "gsm":
+            newReservation.gsm = newValue;
+            break;
+         case "isBancontact":
+            newReservation.isBancontact = newValue;
+            break;
+         case "isReceived":
+            newReservation.isReceived = newValue;
+            break;
+         case "modele":
+            newReservation.modele = newValue;
+            break;
+         case "montant":
+            newReservation.montant = newValue;
+            break;
+         case "nom":
+            newReservation.nom = newValue;
+            break;
+         case "prenom":
+            newReservation.prenom = newValue;
+            break;
+         case "reservationNumber":
+            newReservation.reservationNumber = newValue;
+            break;
+         case "societe":
+            newReservation.societe = newValue;
+            break;
+         default:
+            break;
+      }
+      editReservation.current = newReservation;
+   };
+
+   const updateReservationFirebase = (): void => {
+      reservation.current = { ...editReservation.current };
+      setEditMode(false);
+      if (reservation.current.id) {
+         updateReservationAsync(reservation.current.id, reservation.current);
+      }
+   };
+
+   const toggleToEditMode = () => {
+      setEditMode(true);
+      editReservation.current = { ...reservation.current };
+   };
+
+   const renderAccessoires: Array<ReactElement> = reservation.current.accessoires
+      ? reservation.current.accessoires.map((accessoire, index) => {
            return <Chip label={accessoire} variant="outlined" color="secondary" size="medium" key={index} />;
         })
       : [];
 
-   return (
-      <>
+   if (editMode && editReservation.current) {
+      return (
          <Dialog open={open} onClose={onClose_} fullWidth scroll="paper">
             <DialogTitle>Reservation</DialogTitle>
+            <Divider />
             <DialogContent>
-               <Grid container className={classes.dialog} direction="column">
-                  <Grid item>
-                     <Typography className={classes.title}>Societé:</Typography>
-                     <Typography>{reservation.societe}</Typography>
+               <Grid container className={classes.dialog} direction="row" justify="space-between">
+                  <Grid item xs={12}>
+                     <TextComponent
+                        placeholder="Dossier Vary"
+                        onChange={e => updateReservation("reservationNumber", e)}
+                        value={editReservation.current.reservationNumber}
+                        variant="standard"
+                        customClass={{ width: "50%" }}
+                     />
                   </Grid>
-                  <Grid item>
-                     <Typography className={classes.title}>Machine:</Typography>
-                     <Typography>{reservation.modele}</Typography>
+                  <Grid item xs={12}>
+                     <TextComponent
+                        placeholder="Societé"
+                        onChange={e => updateReservation("societe", e)}
+                        value={editReservation.current.societe}
+                        variant="standard"
+                        customClass={{ width: "50%" }}
+                     />
                   </Grid>
-                  <Grid item>
-                     <Typography className={classes.title}>Accessoires:</Typography>
-                     {renderAccessoires}
+                  <Grid item xs={12}>
+                     <TextComponent
+                        placeholder="Machine"
+                        onChange={e => updateReservation("modele", e)}
+                        value={editReservation.current.modele}
+                        variant="standard"
+                        customClass={{ width: "50%" }}
+                     />
                   </Grid>
-                  <Grid item>
-                     <Typography className={classes.title}>Addresse:</Typography>
-                     <Typography>{reservation.address}</Typography>
+                  <Grid item xs={12}>
+                     <TextComponent
+                        placeholder="Accessoires"
+                        multiple
+                        onChange={e => updateReservation("accessoires", e)}
+                        value={editReservation.current.accessoires}
+                        variant="standard"
+                        customClass={{ width: "90%" }}
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextComponent
+                        placeholder="Addresse"
+                        onChange={e => updateReservation("address", e)}
+                        value={editReservation.current.address}
+                        variant="standard"
+                        customClass={{ width: "50%" }}
+                     />
                   </Grid>
                </Grid>
             </DialogContent>
+            <Divider />
+            <DialogActions>
+               <Button onClick={() => setEditMode(false)} variant="outlined" color="secondary">
+                  Retour
+               </Button>
+               <Button onClick={updateReservationFirebase} variant="outlined">
+                  Enregistrer
+               </Button>
+            </DialogActions>
          </Dialog>
-      </>
+      );
+   }
+
+   return (
+      <Dialog open={open} onClose={onClose_} fullWidth scroll="paper">
+         <DialogTitle>Reservation</DialogTitle>
+         <Divider />
+         <DialogContent>
+            <Grid container className={classes.dialog} direction="column">
+               <Grid item>
+                  <Typography className={classes.title}>Dossier Vary:</Typography>
+                  <Typography>{reservation.current.reservationNumber}</Typography>
+               </Grid>
+               <Grid item>
+                  <Typography className={classes.title}>Societé:</Typography>
+                  <Typography>{reservation.current.societe}</Typography>
+               </Grid>
+               <Grid item>
+                  <Typography className={classes.title}>Machine:</Typography>
+                  <Typography>{reservation.current.modele}</Typography>
+               </Grid>
+               <Grid item>
+                  <Typography className={classes.title}>Accessoires:</Typography>
+                  {renderAccessoires}
+               </Grid>
+               <Grid item>
+                  <Typography className={classes.title}>Addresse:</Typography>
+                  <Typography>{reservation.current.address}</Typography>
+               </Grid>
+            </Grid>
+         </DialogContent>
+         <Divider />
+         <DialogActions>
+            <Button onClick={toggleToEditMode} variant="outlined" color="secondary">
+               Modifier
+            </Button>
+         </DialogActions>
+      </Dialog>
    );
 };
 
