ad7f281452e535b07ddd8ff820cf188d111c0195
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-20 07:30:21 +0200

    Add calendar filters

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,5 @@
 import { Optional, Nullable } from "./Interfaces/Common";
+import { Reservation } from "./components/reservation_form";
 
 export const isFunction = (f: any): f is Function => {
    return typeof f === "function";
@@ -13,3 +14,37 @@ export const checkIfConnected = (user: Optional<Nullable<firebase.User>>, path: 
 export interface WithId {
    id: string;
 }
+
+export const isPreparation = (reservation: Reservation): boolean => {
+   switch (reservation.type) {
+      case "Fini":
+         return true;
+      case "Livraison":
+         return false;
+      case "Livre":
+         return false;
+      case "Preparation":
+         return true;
+      case "Retour":
+         return false;
+      default:
+         return true;
+   }
+};
+
+export const isLivraison = (reservation: Reservation): boolean => {
+   switch (reservation.type) {
+      case "Fini":
+         return true;
+      case "Livraison":
+         return true;
+      case "Livre":
+         return true;
+      case "Preparation":
+         return false;
+      case "Retour":
+         return true;
+      default:
+         return true;
+   }
+};
