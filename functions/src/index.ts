b651781df520db567db337afab08f5df63b8d9cb
Maxime Antoine <maximeantoine1997@gmail.com>
2020-04-27 08:00:38 +0200

    update startDate & endDate type

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from "firebase-functions";
-import * as moment from "moment";
+// import * as moment from "moment";
 import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 
 admin.initializeApp();
+
+const app = express();
+app.use(cors());
 
 export interface Reservation {
    id?: string;
@@ -14,86 +19,170 @@ export interface Reservation {
    gsm: string;
    email: string;
    address: string;
-   startDate: moment.Moment;
-   endDate?: moment.Moment;
+   startDate: string;
+   endDate?: string;
    montant: number;
    isBancontact: boolean;
    isReceived: boolean;
 }
+// const getWeekReservationsAsync = functions.https.onCall(
+//    async (data: any, context): Promise<Array<Array<Reservation>>> => {
+//       const date: string = data.date;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//       const weekReservations: Array<Array<Reservation>> = [[], [], [], [], [], [], []];
 
-export const getWeekReservationsAsync = functions.https.onCall(
-   async (data: any, context): Promise<Array<Array<Reservation>>> => {
-      const date: string = data.date;
+//       const cache: Array<Record<string, Reservation>> = [];
 
-      const weekReservations: Array<Array<Reservation>> = [[], [], [], [], [], [], []];
+//       let i = 1;
+//       while (i < 8) {
+//          const currentIndex = i;
 
-      const cache: Array<Record<string, Reservation>> = [];
+//          const currentDate = moment(date).day(i);
 
-      let i = 1;
-      while (i < 8) {
-         const currentIndex = i;
+//          admin
+//             .database()
+//             .ref("Calendar/")
+//             .child(currentDate.format("YYYY-MM-DD"))
+//             .on("value", snapshot => {
+//                if (snapshot) {
+//                   const reservations = snapshot.val();
 
-         const currentDate = moment(date).day(i);
+//                   if (reservations) {
+//                      const reservationIds: Array<string> = Object.values(reservations);
 
-         await admin
-            .database()
-            .ref("Calendar/")
-            .child(currentDate.format("YYYY-MM-DD"))
-            .once("value")
-            .then(snapshot => {
-               if (snapshot) {
-                  const reservations = snapshot.val();
+//                      reservationIds.forEach(async id => {
+//                         let reservation = cache.find(record => {
+//                            return record[id];
+//                         });
+//                         if (!reservation) {
+//                            admin
+//                               .database()
+//                               .ref("Reservations")
+//                               .child(id)
+//                               .on("value", snap => {
+//                                  if (snap) {
+//                                     const res = snap.val();
+//                                     // put res in cache
+//                                     const cacheRes: Record<string, Reservation> = {};
+//                                     cacheRes[id] = JSON.parse(res);
+//                                     cache.push(cacheRes);
+//                                     reservation = cacheRes;
+//                                  }
+//                               });
+//                         }
 
-                  if (reservations) {
-                     const reservationIds: Array<string> = Object.values(reservations);
+//                         if (reservation) {
+//                            const finalReservation = Object.values(reservation)[0];
+//                            weekReservations[currentIndex - 1].push(finalReservation);
+//                         }
+//                      });
+//                   }
+//                }
+//             });
+//          i++;
+//       }
 
-                     reservationIds.forEach(async id => {
-                        let reservation = cache.find(record => {
-                           return record[id];
-                        });
-                        if (!reservation) {
-                           await admin
-                              .database()
-                              .ref("Reservations")
-                              .child(id)
-                              .once("value")
-                              .then(snap => {
-                                 const res = snap.val();
-                                 // put res in cache
-                                 const cacheRes: Record<string, Reservation> = {};
-                                 cacheRes[id] = JSON.parse(res);
-                                 cache.push(cacheRes);
-                                 reservation = cacheRes;
-                              })
-                              .catch(error => {
-                                 // Re-throwing the error as an HttpsError so that the client gets the error details.
-                                 throw new functions.https.HttpsError("unknown", error.message, error);
-                              });
-                        }
+//       const final = Promise.all(weekReservations);
+//       return final;
+//    }
+// );
 
-                        if (reservation) {
-                           const finalReservation = Object.values(reservation)[0];
-                           weekReservations[currentIndex - 1].push(finalReservation);
-                        }
+app.get("/calendar", (req, res) => {
+   console.log("req is: ", req);
+   const date: string = req.query.date as string;
+   console.log("the date is: ", date);
+   admin
+      .firestore()
+      .collection("Calendar")
+      .doc(date)
+      .get()
+      .then(snap => {
+         let ids: Array<string> = [];
+         console.log("snap data is: ", snap.data());
+         const data = snap.data();
+         if (data) {
+            ids = Object.values<string>(data);
+         }
+         return res.json(ids);
+      })
+      .catch(error => {
+         console.log(error);
+         return res.json({ error: "an error occured" });
+      });
+});
+
+app.get("/todayreservations", (req, res) => {
+   console.log("req is: ", req);
+   const date: string = req.query.date as string;
+   console.log("the date is: ", date);
+   admin
+      .firestore()
+      .collection("Calendar")
+      .doc(date)
+      .get()
+      .then(async snapshot => {
+         let result: Array<string> = [];
+         console.log("snap data is: ", snapshot.data());
+         const data = snapshot.data();
+         if (data) {
+            const reservations = data.res;
+
+            result = await admin
+               .firestore()
+               .collection("Reservations")
+               .where(admin.firestore.FieldPath.documentId(), "in", reservations)
+               .get()
+               .then(snap => {
+                  console.log("reservations are: ", snap.docs);
+                  if (snap.docs) {
+                     snap.docs.forEach(doc => {
+                        console.log("doc data: ", doc.data());
                      });
                   }
-               }
-            })
-            .catch(error => {
-               // Re-throwing the error as an HttpsError so that the client gets the error details.
-               throw new functions.https.HttpsError("unknown", error.message, error);
-            });
-         i++;
-      }
+                  return [];
+               });
+         }
+         return res.json(result);
+      })
+      .catch(error => {
+         console.log(error);
+         return res.json({ error: "an error occured" });
+      });
+});
 
-      const final = Promise.all(weekReservations);
-      return final;
-   }
-);
+app.post("/reservation", (req, res) => {
+   const newReservation: Reservation = {
+      prenom: req.body.prenom,
+      id: req.body.id || "",
+      nom: req.body.nom,
+      societe: req.body.societe,
+      modele: req.body.modele,
+      accessoires: req.body.accessoires,
+      gsm: req.body.gsm,
+      email: req.body.email,
+      address: req.body.address,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate || "",
+      montant: req.body.montant,
+      isBancontact: req.body.isBancontact,
+      isReceived: req.body.isReceived,
+   };
+
+   console.log("the data is: ", newReservation);
+   admin
+      .firestore()
+      .collection("calendar")
+      .doc(newReservation.startDate)
+      .collection("reservations")
+      .doc()
+      .set(newReservation)
+      .then(() => {
+         return res.status(200).send({ message: "Reservation was made successfuly" });
+      })
+      .catch(error => {
+         console.log(error);
+         return res.status(500).send({ message: "An error occured" });
+      });
+});
+
+export const api = functions.region("europe-west1").https.onRequest(app);
