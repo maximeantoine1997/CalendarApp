d8600a7d684b0490c7a1f0ea8842b676d67e3361
Maxime Antoine <maximeantoine1997@gmail.com>
2020-05-10 10:43:28 +0200

    Add typeColors

--- src/Interfaces/Common.ts
+++ src/Interfaces/Common.ts
@@ -2,4 +2,14 @@ export type Nullable<T> = null | T;
 
 export type Optional<T> = undefined | T;
 
-export type CalendarType = "livraison" | "preparation";
+export type CalendarType = "livraison" | "preparation" | "livre";
+
+export type ReservationType = "Preparation" | "Livraison" | "Livre" | "Retour" | "Fini";
+
+export const typeColors: Record<ReservationType, string> = {
+   Preparation: "#E09494",
+   Livraison: "#D7E6CB",
+   Livre: "#DEE3F1",
+   Retour: "#DEE3F1",
+   Fini: "#00000",
+};
