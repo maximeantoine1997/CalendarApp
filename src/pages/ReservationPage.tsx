06f256b9a4ac26c8d134328a8f0abb991cb029ac
Maxime Antoine <maximeantoine1997@gmail.com>
2020-05-19 07:29:08 +0200

    Update layout

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,34 +1,8 @@
-import { createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
-
 import React from "react";
-import { Redirect } from "react-router-dom";
-import useUserContext from "../Contexts/UserContext";
 import ReservationForm from "../components/reservation_form";
 
-const useStyles = makeStyles((theme: Theme) =>
-   createStyles({
-      grid: {
-         height: "100%",
-         // This makes sure that the content doesn't go under the navbar
-         paddingTop: "5vh",
-      },
-   })
-);
-
 const ReservationPage = () => {
-   const classes = useStyles();
-
-   const { user } = useUserContext();
-
-   if (user) {
-      return (
-         <Grid container justify="center" alignContent="center" alignItems="center" className={classes.grid}>
-            <ReservationForm />
-         </Grid>
-      );
-   }
-
-   return <Redirect to="/" />;
+   return <ReservationForm />;
 };
 
 export default ReservationPage;
