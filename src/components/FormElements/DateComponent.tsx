f02a3928ff4de4b41b3299f780e3c576d9cfc184
Maxime Antoine <maximeantoine1997@gmail.com>
2020-06-09 06:30:59 +0200

    Add isReadonly state

--- src/components/FormElements/DateComponent.tsx
+++ src/components/FormElements/DateComponent.tsx
@@ -1,5 +1,5 @@
 import MomentUtils from "@date-io/moment";
-import { Box } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 import moment, { Moment } from "moment";
@@ -9,19 +9,37 @@ import React, { FunctionComponent, useState } from "react";
 moment.locale("fr");
 
 interface DateProps {
+   isReadOnly?: boolean;
    onChange: (date: MaterialUiPickersDate) => void;
    placeholder: string;
    value?: Moment;
 }
 
-const DateComponent: FunctionComponent<DateProps> = ({ onChange: onChange_, placeholder, value = moment() }) => {
-   const [selectedDate, handleDateChange] = useState<MaterialUiPickersDate>(value);
+const DateComponent: FunctionComponent<DateProps> = ({
+   onChange: onChange_,
+   placeholder,
+   value = moment().format("DD/MM/YYYY"),
+   isReadOnly = false,
+}) => {
+   const [selectedDate, handleDateChange] = useState<MaterialUiPickersDate>(moment(value));
 
    const onChange = (date: MaterialUiPickersDate) => {
       handleDateChange(date);
       onChange_(date);
    };
 
+   if (isReadOnly) {
+      return (
+         <Grid container style={{ paddingTop: "10px" }}>
+            <Grid item xs={12}>
+               <Typography style={{ fontWeight: "bold" }}>{placeholder.toUpperCase()}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+               <Typography>{moment(value).format("DD/MM/YYYY")}</Typography>
+            </Grid>
+         </Grid>
+      );
+   }
    return (
       <Box width="100%">
          <MuiPickersUtilsProvider utils={MomentUtils} locale="fr">
