8bffceb9601853926896a391d1307f5093e78dde
Maxime Antoine <maximeantoine1997@gmail.com>
2020-07-15 07:27:15 +0200

    Add notes to weekview

--- src/components/Calendar/CalendarWeekView.tsx
+++ src/components/Calendar/CalendarWeekView.tsx
@@ -1,40 +1,52 @@
 import { Grid } from "@material-ui/core";
 import "firebase/functions";
-import { useSnackbar } from "notistack";
 import React, { useEffect } from "react";
 import useCalendarContext from "../../Contexts/CalendarContext";
-import { getReservations, Fauna, convertToReservation } from "../../FaunaDB/Api";
+import { convertToNote, convertToReservation, Fauna, getNotes, getReservations, Note } from "../../FaunaDB/Api";
 import { getWeekDays, HashMap } from "../../Utils";
 import { Reservation } from "../reservation_form";
 import CalendarWeekTab from "./CalendarWeekTab";
 
 const CalendarWeekView: React.FC = () => {
-   const { date, setReservations } = useCalendarContext();
-   const { enqueueSnackbar } = useSnackbar();
+   const { date, setReservations, setNotes } = useCalendarContext();
 
    useEffect(() => {
       const getData = async () => {
-         getReservations(getWeekDays(date)).then((newReservations: any) => {
-            if (!newReservations) return;
-            const hash: HashMap<Reservation> = {};
-            newReservations.forEach((res: Fauna<Reservation>) => {
-               const reservation: Reservation = convertToReservation(res);
-               const start = reservation.startDate;
-               const startData = hash[start];
+         const newReservations: any = await getReservations(getWeekDays(date));
+         const newNotes: any = await getNotes(getWeekDays(date));
+         if (!newReservations || !newNotes) return;
+         const hasReservations: HashMap<Reservation> = {};
+         const hashNotes: HashMap<Note> = {};
+         newReservations.forEach((res: Fauna<Reservation>) => {
+            const reservation: Reservation = convertToReservation(res);
+            const start = reservation.startDate;
+            const startData = hasReservations[start];
 
-               if (startData) {
-                  startData.push(reservation);
-                  hash[start] = startData;
-               } else {
-                  hash[start] = [reservation];
-               }
-            });
-
-            setReservations(hash);
+            if (startData) {
+               startData.push(reservation);
+               hasReservations[start] = startData;
+            } else {
+               hasReservations[start] = [reservation];
+            }
          });
+         newNotes.forEach((res: Fauna<Note>) => {
+            const note: Note = convertToNote(res);
+            const start = note.date;
+            const startData = hashNotes[start];
+
+            if (startData) {
+               startData.push(note);
+               hashNotes[start] = startData;
+            } else {
+               hashNotes[start] = [note];
+            }
+         });
+         console.log(hashNotes);
+         setReservations(hasReservations);
+         setNotes(hashNotes);
       };
       getData();
-   }, [date, enqueueSnackbar, setReservations]);
+   }, [date, setNotes, setReservations]);
 
    const weekDays = getWeekDays(date);
 
