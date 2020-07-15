73dca672cec868840daf707ac7bae884ff2e4212
Maxime Antoine <maximeantoine1997@gmail.com>
2020-07-15 07:27:27 +0200

    Remove unused sidebar

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -1,22 +1,5 @@
-import {
-   AppBar,
-   createStyles,
-   Drawer,
-   IconButton,
-   List,
-   ListItem,
-   ListItemIcon,
-   ListItemText,
-   makeStyles,
-   Theme,
-   Toolbar,
-   Typography,
-} from "@material-ui/core";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import AddIcon from "@material-ui/icons/Add";
-import EventNoteIcon from "@material-ui/icons/EventNote";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState } from "react";
+import { AppBar, createStyles, makeStyles, Theme, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
 import { Link } from "react-router-dom";
 import Authentification from "./Authentification";
 
@@ -46,11 +29,11 @@ const useStyles = makeStyles((theme: Theme) =>
       title: {
          flexGrow: 1,
          textDecoration: "none",
-         color: "white",
+         color: "black",
       },
       nav: {
-         background: "#16181F",
-         color: "white",
+         background: "white",
+         color: "black",
       },
       profileMenu: {
          marginTop: "50px",
@@ -61,25 +44,10 @@ const useStyles = makeStyles((theme: Theme) =>
 const SideBar: React.FC = () => {
    const classes = useStyles();
 
-   const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
-
-   const toggleDrawer = (value: boolean) => {
-      setIsOpenDrawer(value);
-   };
-
    return (
       <>
          <AppBar position="sticky" className={classes.nav}>
             <Toolbar>
-               <IconButton
-                  edge="start"
-                  className={classes.menuButton}
-                  color="inherit"
-                  aria-label="menu"
-                  onClick={() => toggleDrawer(true)}
-               >
-                  <MenuIcon />
-               </IconButton>
                <Typography variant="h6" className={classes.title}>
                   <Link to="/" className={classes.title}>
                      Agenda Antoine SPRL
@@ -88,40 +56,6 @@ const SideBar: React.FC = () => {
                <Authentification />
             </Toolbar>
          </AppBar>
-         <Drawer anchor="left" open={isOpenDrawer} onClose={() => toggleDrawer(false)}>
-            <List component="nav" aria-label="navigation menu" className={classes.list}>
-               <Link to="/account" className={classes.link} onClick={() => toggleDrawer(false)}>
-                  <ListItem button dense className={classes.listItem}>
-                     <ListItemIcon>
-                        <AccountCircleIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText>
-                        <Typography variant="h6">Mon Compte</Typography>
-                     </ListItemText>
-                  </ListItem>
-               </Link>
-               <Link to="/calendrier" className={classes.link} onClick={() => toggleDrawer(false)}>
-                  <ListItem button dense className={classes.listItem}>
-                     <ListItemIcon>
-                        <EventNoteIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText>
-                        <Typography variant="h6">Calendrier</Typography>
-                     </ListItemText>
-                  </ListItem>
-               </Link>
-               <Link to="/reservation" className={classes.link} onClick={() => toggleDrawer(false)}>
-                  <ListItem button dense className={classes.listItem}>
-                     <ListItemIcon>
-                        <AddIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText>
-                        <Typography variant="h6">Reservation</Typography>
-                     </ListItemText>
-                  </ListItem>
-               </Link>
-            </List>
-         </Drawer>
       </>
    );
 };
