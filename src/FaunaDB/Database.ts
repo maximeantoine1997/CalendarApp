abee464fe5491e5eba3fa70b46cdbf9cad609d03
Maxime Antoine <maximeantoine1997@gmail.com>
2020-07-15 08:48:20 +0200

    remove useless comments

--- src/FaunaDB/Database.ts
+++ src/FaunaDB/Database.ts
@@ -1,14 +1,6 @@
 import faunadb, { query as q } from "faunadb";
 
-//
-
-//new
 const secret = process.env.REACT_APP_FAUNADB_API_KEY || "";
-// const user = localStorage.getItem("authUser");
-// if (user) {
-//    secret = JSON.parse(user).secret;
-//    console.log(secret);
-// }
 
 const client = new faunadb.Client({ secret: secret });
 
