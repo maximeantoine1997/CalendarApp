5723736e59e7983c28b7a8efac8b4296e1b8f1e9
Maxime Antoine <maximeantoine1997@gmail.com>
2020-07-15 08:48:57 +0200

    Update modal layout & logic

--- src/components/Calendar/Notes/NoteModal.tsx
+++ src/components/Calendar/Notes/NoteModal.tsx
@@ -1,9 +1,38 @@
-import { Button, Dialog, DialogActions, DialogContent, Divider, Grid } from "@material-ui/core";
+import {
+   Button,
+   Dialog,
+   DialogActions,
+   DialogContent,
+   Divider,
+   Grid,
+   Typography,
+   makeStyles,
+   createStyles,
+   Box,
+} from "@material-ui/core";
 import { useSnackbar } from "notistack";
 import React, { ReactNode, useEffect, useState } from "react";
 import useCalendarContext from "../../../Contexts/CalendarContext";
-import { convertToNote, Fauna, FDBcreateNotesAsync, FDBupdateNotesAsync, Note } from "../../../FaunaDB/Api";
+import {
+   convertToNote,
+   Fauna,
+   FDBcreateNotesAsync,
+   FDBupdateNotesAsync,
+   Note,
+   FDBDeleteNotesAsync,
+} from "../../../FaunaDB/Api";
 import TextComponent from "../../FormElements/TextComponent";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyles = makeStyles(() =>
+   createStyles({
+      delete: {
+         paddingTop: "15px",
+         cursor: "pointer",
+      },
+   })
+);
+
 interface NoteModalProps {
    open: boolean;
    onClose: () => void;
@@ -11,13 +40,17 @@ interface NoteModalProps {
 }
 
 const NoteModal: React.FunctionComponent<NoteModalProps> = ({ open: open_, onClose: onClose_, day: day_ }) => {
+   const classes = useStyles();
    const { enqueueSnackbar } = useSnackbar();
    const { notes: notes_, setNotes: setNotes_ } = useCalendarContext();
 
    const [isReadOnly, setIsReadOnly] = useState<boolean>(true);
    const [notes, setNotes] = useState<Array<Note>>(notes_[day_] || []);
 
-   const onModify = () => {
+   const onModify = async () => {
+      if (!notes.length) {
+         await onAdd();
+      }
       setIsReadOnly(false);
    };
 
@@ -29,9 +62,12 @@ const NoteModal: React.FunctionComponent<NoteModalProps> = ({ open: open_, onClo
    };
 
    const onSave = () => {
+      // Update notes from DB
       notes.forEach(note => {
          FDBupdateNotesAsync(note);
       });
+
+      // Update notes from React Context
       setNotes_(prev => {
          prev[day_] = notes;
          return prev;
@@ -54,6 +90,26 @@ const NoteModal: React.FunctionComponent<NoteModalProps> = ({ open: open_, onClo
       });
    };
 
+   const onDelete = async (id: string) => {
+      const index = notes.findIndex(element => element.id === id);
+      if (index < 0) return;
+      // Remove note from DB
+      await FDBDeleteNotesAsync(notes[index]);
+
+      // Remove note from React
+      setNotes(prev => {
+         prev.splice(index, 1);
+         return [...prev];
+      });
+
+      enqueueSnackbar("Supprimé", { variant: "success" });
+   };
+
+   const onClose = () => {
+      setIsReadOnly(true);
+      onClose_();
+   };
+
    useEffect(() => {
       if (notes_[day_]) {
          setNotes(notes_[day_]);
@@ -62,46 +118,90 @@ const NoteModal: React.FunctionComponent<NoteModalProps> = ({ open: open_, onClo
 
    const renderNote = (note: Note): ReactNode => {
       return (
-         <Grid key={note.id} container justify="space-around">
-            <Grid item xs={3}>
+         <Grid key={note.id} container justify="space-evenly" alignItems="center">
+            <Grid item xs={isReadOnly ? 4 : 3}>
                <TextComponent
+                  customClass={{ width: "80%" }}
                   placeholder="Nom"
                   onChange={e => onChange("name", e, note.id as string)}
                   isRequired
                   value={note.name}
                   isReadOnly={isReadOnly}
+                  noReadOnlyText
                />
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={isReadOnly ? 4 : 3}>
                <TextComponent
+                  customClass={{ width: "80%" }}
                   placeholder="Raison"
                   onChange={e => onChange("type", e, note.id as string)}
                   isRequired
                   value={note.type}
                   isReadOnly={isReadOnly}
+                  noReadOnlyText
                />
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={isReadOnly ? 4 : 3} style={{ width: "80%" }}>
                <TextComponent
+                  customClass={{ width: "80%" }}
                   placeholder="Description"
                   onChange={e => onChange("note", e, note.id as string)}
                   value={note.note}
                   isReadOnly={isReadOnly}
+                  noReadOnlyText
                />
             </Grid>
+            {!isReadOnly && (
+               <Grid item xs={1}>
+                  <DeleteIcon className={classes.delete} onClick={() => onDelete(note.id as string)} />
+               </Grid>
+            )}
          </Grid>
       );
    };
 
+   const renderNoteDescription = isReadOnly ? (
+      <Grid container justify="space-evenly">
+         <Grid item xs={isReadOnly ? 4 : 3}>
+            <Typography variant="h6" style={{ fontWeight: "bold" }}>
+               Nom
+            </Typography>
+         </Grid>
+         <Grid item xs={isReadOnly ? 4 : 3}>
+            <Typography variant="h6" style={{ fontWeight: "bold" }}>
+               Raison
+            </Typography>
+         </Grid>
+         <Grid item xs={isReadOnly ? 4 : 3}>
+            <Typography variant="h6" style={{ width: "80%", fontWeight: "bold" }}>
+               Description
+            </Typography>
+         </Grid>
+         {!isReadOnly && <Grid item xs={1}></Grid>}
+      </Grid>
+   ) : null;
+
    return (
       <>
-         <Dialog open={open_} onClose={onClose_} fullWidth maxWidth="md" scroll="paper">
+         <Dialog open={open_} onClose={onClose} fullWidth maxWidth="md" scroll="paper">
             <DialogContent>
-               {notes?.map(note => renderNote(note))}
+               {renderNoteDescription}
+               {notes?.map((note, index) =>
+                  index + 1 === notes.length ? (
+                     renderNote(note)
+                  ) : (
+                     <Box key={note.id}>
+                        {renderNote(note)} <Divider />
+                     </Box>
+                  )
+               )}
+
                {!isReadOnly && (
-                  <Button variant="outlined" color="secondary" onClick={onAdd}>
-                     Add +
-                  </Button>
+                  <Grid container justify="flex-end">
+                     <Button variant="outlined" color="secondary" onClick={onAdd}>
+                        Ajouter +
+                     </Button>
+                  </Grid>
                )}
             </DialogContent>
             <Divider />
