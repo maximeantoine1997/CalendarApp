c5ea3b752eda6582c71ea08c5adb40b70e325031
Maxime Antoine <maximeantoine1997@gmail.com>
2020-11-13 11:16:39 -0500

    Add autocompleteReverseMapping

--- src/FaunaDB/FaunaDB.Utils.ts
+++ src/FaunaDB/FaunaDB.Utils.ts
@@ -11,3 +11,17 @@ export const autocompleteMapping: Record<string, string> = {
    "10": "sitePhone",
    "11": "street",
 };
+
+export const autocompleteReverseMapping: Record<string, string> = {
+    "VATNumber": "1" ,
+    "accessoires": "2",
+    "city": "3",
+    "company": "4",
+    "email": "5",
+    "modele": "6",
+    "name": "7",
+    "phone": "8",
+    "postalCode": "9",
+    "sitePhone": "10",
+    "street": "11",
+ };
